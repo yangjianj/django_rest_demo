c722c7dbd52bf67581663d129a1ff328560faf94
yangjianj <252089024@qq.com>
2020-05-06 10:23:28 +0800

    update

--- normal_app/views.py
+++ normal_app/views.py
@@ -1,16 +1,26 @@
 from django.shortcuts import render
 import json
-from django.views.generic.base import View
+from django.views.generic import View
 from django.http import HttpResponse,JsonResponse
 # Create your views here.
 
 class CourseView(View):
 	def get(self,request):
-		data = {"a":1}
+		data = '{"a":1}'
 		json_data = json.loads(data)
 		return JsonResponse(json_data)
 	
 	def post(self,request):
-		data = {"a":1}
+		data = '{"a":1}'
+		json_data = json.loads(data)
+		return JsonResponse(json_data)
+
+	def put(self,request):
+		data = '{"a":1}'
+		json_data = json.loads(data)
+		return JsonResponse(json_data)
+
+	def delete(self,request):
+		data = '{"a":1}'
 		json_data = json.loads(data)
 		return JsonResponse(json_data)
